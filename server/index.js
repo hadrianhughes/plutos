6a742c29e8bb88adb25e7db98fae3ed8b7b1cf17
Hadrian Hughes <h.hughes@amaze.com>
2017-09-05 23:17:56 +0100

    Polish header component styling.

--- server/index.js
+++ server/index.js
@@ -12,6 +12,7 @@ app.get('/', (req, res) => {
 	<html>
 		<head>
 			<title>Plutos</title>
+			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		</head>
 		<body>
 			<div id="app"></div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,15 +7,6 @@ const Header = ({navItems}) => (
     <h1 className="header__logo">
       Plutos
     </h1>
-    <nav className="header__nav">
-      <ul className="header__nav-list">
-        {navItems.map((item, index) => (
-          <li key={index} className="header__nav-item">
-            <a href={item.href} target="_self" className="header__nav-link">{item.text}</a>
-          </li>
-        ))}
-      </ul>
-    </nav>
   </header>
 );
 

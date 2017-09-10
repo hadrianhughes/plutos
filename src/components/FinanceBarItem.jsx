b8483f0a105ab568bd2366c7afae50b776700da8
Hadrian Hughes <h.hughes@amaze.com>
2017-09-10 22:34:12 +0100

    Create FinanceBarItem details.

--- src/components/FinanceBarItem.jsx
+++ src/components/FinanceBarItem.jsx
@@ -12,6 +12,15 @@ const FinanceBarItem = ({
   color
 }) => (
   <div className="FinanceBarItem" style={{width: portion + '%', backgroundColor: color}}>
+    <div className="FinanceBarItem__details-bubble">
+      <ul className="FinanceBarItem__details">
+        <li className="FinanceBarItem__detail">{name}</li>
+        <li className="FinanceBarItem__detail">{amount}</li>
+        <li className="FinanceBarItem__detail">{period}</li>
+        <li className="FinanceBarItem__detail">{source}</li>
+        <li className="FinanceBarItem__detail">{note}</li>
+      </ul>
+    </div>
   </div>
 );
 
